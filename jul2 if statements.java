c53ab7a958678737354085b06ef91e985e25b233
Chowhitha Anantha <chowhitha@gmail.com>
2026-07-02 09:57:22 +0530

    Update jul2 if statements.java

--- jul2 if statements.java
+++ jul2 if statements.java
@@ -1,3 +1,4 @@
+//checking login
 import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
@@ -8,12 +9,26 @@ public class Main {
     String uname = sc.nextLine();
     System.out.println("pwd");
     String pd = sc.nextLine();
-    if((username.equals(uname)) && (pwd.equals(pd))){
+    if((username.equals(uname)) && (pwd.equals(pd))){        //when comparing strings, always use equals instead "=="; "==" compares the memory of the string
       System.out.println("login successful");
-    } else if((username == uname) && (pwd!=pd)){
+    } else if((username == uname) && (pwd!=pd)){   //wrong
       System.out.println("incorrect pwd");
     } else{
       System.out.println("error");
     }
   }
+} 
+
+
+//odd or even
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("number");
+    int a = sc.nextInt();
+    String mod = (a%2 == 0)? "even" : "odd";
+    System.out.println(mod);
+  }
 }
+
