1331632ae461387e5f6a8162f4c8ba2b15afa703
Chowhitha Anantha <chowhitha@gmail.com>
2026-07-02 11:40:50 +0530

    Implement odd and even number printing
    Added functionality to print odd and even numbers up to a user-defined limit.

--- jul2.java
+++ jul2.java
@@ -77,3 +77,25 @@ public class Main {
     }
 }
 
+//
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		System.out.println("odd: ");
+		for(int i =0;i<=x;i++){
+		    if(i%2 != 0){
+		        System.out.println( i);
+		    }
+		}
+		System.out.println("even: ");
+		for(int j =0;j<=x;j++){
+		    if(j%2 == 0){
+		        System.out.println(j);
+		    }
+		}
+		System.out.println("Hello World");
+	}
+}
+
