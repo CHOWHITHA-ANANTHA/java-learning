bfd0b15795bd2a13c05d8b1a4c5cb99952974b47
Chowhitha Anantha <chowhitha@gmail.com>
2026-07-04 11:11:27 +0530

    //class, objects, constructors, local vs gloabal variables //array index change

--- jul4.java
+++ jul4.java
@@ -1,5 +1,11 @@
 
 //class, objects, constructors, local vs gloabal variables
+//array index change
+
+
+
+
+
 class hosp{         //class
     String dname;           //global variable cz its in class, not in fn so it can be used in the entire programme
     String name;
@@ -17,3 +23,31 @@ public class Main{
 		System.out.println(p1.name);
 	}
 }
+
+//array index change
+public class Main {
+    public static void main(String[] args) {
+        int[][] m = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
+        };
+        for (int i = 0; i < m.length; i++) {
+            int firstIdx = 0;       //we name first and last indexes are variables to change them
+            int lastIdx = m[i].length - 1;
+            
+            int newm = m[i][firstIdx];          //interchanging indices
+            m[i][firstIdx] = m[i][lastIdx];
+            m[i][lastIdx] = newm;
+        }
+        for (int[] row : m) {
+            System.out.print("[");
+            for (int j = 0; j < row.length; j++) {
+                System.out.print(row[j] + (j < row.length - 1 ? ", " : ""));
+            }
+            System.out.println("]");
+        }
+    }
+}
+
